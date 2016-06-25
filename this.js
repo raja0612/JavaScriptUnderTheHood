c27f4e95111157423b5452b03b63e0b9907856e7
raja0612 <rajashekhar.ramayampeta@gmail.com>
2016-06-25 17:06:56 -0400

    Added this keyword in detail.

--- this.js
+++ this.js
@@ -12,19 +12,42 @@ var b = function(){
     console.log(this.name);
 }
 //here this keyword points to varible c  context location. so c has it's own context area.
-//In below code both console statements prints "Rajashekhar Goud Ramayampeta";
+//In below code both console statements prints "Rajashekhar Goud Ramayampeta" even though we updated name property value;
 var c = {
-       name:"Rajashekhar Goud Ramayampeta",
-       log:function(){
+       name : "Rajashekhar Ramayampeta",
+       log : function(){
+           this.name = "Rajashekhar Goud Ramayampeta";
+           
            console.log(this.name);
+           
            var setname = function(newname){
                this.name = newname;
-               console.log(this.name);
-               setname("Srujan Jack");
            }
+           
+           setname("Srujan Jack");
+           console.log(this.name);
+       }  
+    
+}
+// so in order to resolve above conflict, we created self varible to point current this.
+var d = {
+       name : "Rajashekhar Ramayampeta",
+       log : function(){
+           self = this;
+           self.name = "Rajashekhar Goud Ramayampeta";
+           
+           console.log(self.name);
+           
+           var setname = function(newname){
+               self.name = newname;
+           }
+           
+           setname("Srujan Jack");
+           console.log(self.name);
        }  
     
 }
 a();
 b();
 c.log();
+d.log();
