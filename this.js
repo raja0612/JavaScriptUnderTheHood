a1413aa1f3bb4ac2104b615941203be71a70239a
raja0612 <rajashekhar.ramayampeta@gmail.com>
2016-06-25 16:36:46 -0400

    this keyword inside function behaviour.

--- this.js
+++ this.js
@@ -11,11 +11,17 @@ var b = function(){
     console.log(this);
     console.log(this.name);
 }
-//here this keyword points to varible context location. so c has it's own context area.
+//here this keyword points to varible c  context location. so c has it's own context area.
+//In below code both console statements prints "Rajashekhar Goud Ramayampeta";
 var c = {
        name:"Rajashekhar Goud Ramayampeta",
        log:function(){
            console.log(this.name);
+           var setname = function(newname){
+               this.name = newname;
+               console.log(this.name);
+               setname("Srujan Jack");
+           }
        }  
     
 }
